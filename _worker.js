3bd4897f941133b2ba3b9066fab8d64cd3cd4afd
hometown-TheHeaven <70843426+hk1295@users.noreply.github.com>
2024-01-07 14:48:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '1410e7d6-5efd-4c5d-bca2-8cb092f2bfd7';
 
-const proxyIPs = ['104.28.153.18','104.28.153.16','104.28.153.9','2a09:bac5:cb21:119::1c:26d','2a09:bac5:cb26:154b::21f:39']; //'cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'
+const proxyIPs = ['yxcfcdn.mfgo.cloudns.org']; //'cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
