1079dc91a9daff83a355395cb25d001e613b9480
hometown-TheHeaven <70843426+hk1295@users.noreply.github.com>
2024-01-08 11:31:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -100,8 +100,8 @@ export default {
                     }
                     default:
                         // return new Response('Not found', { status: 404 });
-                        // For any other path, reverse proxy to 'maimai.sega.jp' and return the original response
-                        url.hostname = 'maimai.sega.jp';
+                        // For any other path, reverse proxy to 'tv.apple.com' and return the original response
+                        url.hostname = 'tv.apple.com';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
